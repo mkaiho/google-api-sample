73d0d83f0ebbe03d8e07efdad96a26b27131653d
mkaiho <mkaiho.cgengo@gmail.com>
2021-12-19 02:40:57 +0000

    Modify GBP Notification client infrastructure

--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -3,24 +3,18 @@ package main
 import (
 	"context"
 	"log"
-	"os"
 
 	"github.com/mkaiho/google-api-sample/infrastructure"
 )
 
-const (
-	envKeyGoogleAPIKey = "GOOGLE_API_KEY"
-)
-
 func main() {
-	gbpOptions := infrastructure.NewGBPNotificationOptions()
-	gbpOptions.APIKey = os.Getenv(envKeyGoogleAPIKey)
-	if len(gbpOptions.APIKey) == 0 {
-		log.Fatal("GOOGLE_API_KEY is not set")
-		return
-	}
-	_, err := infrastructure.NewGBPNotification(context.Background(), *gbpOptions)
+	gbpCredentials, err := infrastructure.LoadGBPCredentialEnv()
 	if err != nil {
-		log.Fatalf("Error %v", err)
+		log.Fatalf("Error: %v", err)
+	}
+
+	_, err = infrastructure.NewGBPNotification(context.Background(), gbpCredentials)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
 	}
 }
